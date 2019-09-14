a82b639a4998e2c453e1290a60d72c9542df5f4a
vladisslaww <VfCzYz662903>
2019-09-15 00:43:49 +0300

    WIP

--- client/src/components/Documents/DocumentScreenSideBar.vue
+++ client/src/components/Documents/DocumentScreenSideBar.vue
@@ -1,13 +1,26 @@
 <template>
-
+  <article class="container">
+    <vote-text></vote-text>
+    <vote-chat></vote-chat>
+  </article>
 </template>
 
 <script>
+    import VoteText from "../Vote/VoteText.vue";
+    import VoteChat from "../Vote/VoteChat.vue";
     export default {
-        name: "DocumentScreenar"
+        name: "DocumentScreenSideBar",
+        components: {VoteChat, VoteText}
     }
 </script>
 
 <style scoped>
-
+  .container{
+    display: flex;
+    flex-direction: column;
+    border: 2px solid var(--primary);
+    border-radius: 5px;
+    flex: 1 0 100%;
+    align-self: stretch;
+  }
 </style>
